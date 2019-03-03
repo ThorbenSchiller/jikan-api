14c56dd8ae805390b0e8509bb9492c5734078264
thorbeN <schillerthorben@gmail.com>
2019-03-03 22:45:28 +0100

    added log message in constructor for used endpoint url

--- src/JikanApiClient.ts
+++ src/JikanApiClient.ts
@@ -38,6 +38,7 @@ export class JikanApiClient {
     constructor(fetcher: Fetcher, endpointUrl?: string, logger: Logger = console) {
         this.fetcher = fetcher;
         this.logger = logger;
+        this.logger.info(`using jikan endpoint url \"${endpointUrl}\"`);
         this.endpointUrl = endpointUrl || this.getDefaultUrl();
     }
 
