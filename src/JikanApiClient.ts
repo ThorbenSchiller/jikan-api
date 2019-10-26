038d1666d50b389cef1833f17d2405b850fb161b
thorbeN <schillerthorben@gmail.com>
2019-10-26 11:00:46 +0200

    fix(JikanApiClient): proper log message pro jikan api endpoint in constructor

--- src/JikanApiClient.ts
+++ src/JikanApiClient.ts
@@ -46,7 +46,7 @@ export class JikanApiClient {
     constructor(fetcher: Fetcher, endpointUrl?: string, logger: Logger = console) {
         this.fetcher = fetcher;
         this.logger = logger;
-        this.logger.info(`using jikan endpoint url \"${endpointUrl}\"`);
+        this.logger.info(`using jikan endpoint url "${endpointUrl}"`);
         this.endpointUrl = endpointUrl || this.getDefaultUrl();
     }
 
